4c32dc5b6c4a074faa9873c018765c410853e8f4
dvcode-markjames <markjamesrogelio.dvcode@gmail.com>
2026-02-28 18:32:38 +0800

    feat: initial commit of personal portfolio with dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.html"], // <-- Binago ko 'to. Babasahin na niya LAHAT ng .html files sa folder!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
